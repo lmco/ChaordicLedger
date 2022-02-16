05a193268691f61103a2371609ec9c3cbf89045d
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-02-16 21:41:34 +0000

    Created base-level scripting [ci skip]

--- cleanup/agnostic/cleanup_hyperledger.sh
+++ cleanup/agnostic/cleanup_hyperledger.sh
@@ -4,6 +4,3 @@ docker container ls -a
 docker ps --format "{{.ID}}" | xargs docker container stop
 docker container prune -f
 docker container ls -a
-docker image ls
-docker image prune -af
-docker image ls
