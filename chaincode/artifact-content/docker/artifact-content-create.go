4c4bb685246efcd75fc4738c8a5f475f4df813e9
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-06-13 12:37:33 +0000

    Trying the Go IPFS API

--- chaincode/artifact-content/docker/artifact-content-create.go
+++ chaincode/artifact-content/docker/artifact-content-create.go
@@ -159,14 +159,14 @@ func TryGetURL(url string) {
 	fmt.Println("Trying ", url)
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fatalln(err)
+		fmt.Println(err)
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Fatalln(err)
+		fmt.Println(err)
 	}
 
 	fmt.Println(string(body))
