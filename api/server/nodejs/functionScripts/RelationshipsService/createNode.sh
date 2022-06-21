6a97e2ab44d3bd23ba1a3da88fe6a329637ba463
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-06-21 17:18:08 +0000

    Added initial API-based file retrieval.

--- api/server/nodejs/functionScripts/RelationshipsService/createNode.sh
+++ api/server/nodejs/functionScripts/RelationshipsService/createNode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
 
-response=$(curl -X POST http://localhost:7070 -H 'Content-Type: application/json' -d '{{body}}')
+response=$(curl -X POST http://localhost:5678 -H 'Content-Type: application/json' -d '{{body}}')
 echo $response
