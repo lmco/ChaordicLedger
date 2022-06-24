68daf18d4164c099b0a2eed3e99d37069fd7ef34
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-06-24 16:35:39 +0000

    Enabling log aggregation.

--- monitoring/scripts/monitoring.sh
+++ monitoring/scripts/monitoring.sh
@@ -6,19 +6,33 @@ MONITORING_TMP=${TEMP_DIR}/monitoring
 mkdir -p $MONITORING_TMP
 
 function enable_monitoring() {
+  # Reference: https://helm.elastic.co/
+
   kubectl create namespace dapr-monitoring
   kubectl create namespace dapr-system
   helm repo add --force-update elastic https://helm.elastic.co
   helm repo update
-  helm install elasticsearch elastic/elasticsearch -n dapr-monitoring --set replicas=1
   
+  # Download helm chart, replace image prefix with proxy variables, install.
+  # This allows the use of proxies without affecting the image name and tag.
+  helm pull elastic/elasticsearch --untar --untardir $MONITORING_TMP
+  sed -i "s|docker.elastic.co/|$DOCKER_REGISTRY_PROXY$REGISTRY_ELASTIC_DOCKER|g" $MONITORING_TMP/elasticsearch/values.yaml
+  helm install elasticsearch $MONITORING_TMP/elasticsearch -n dapr-monitoring --set replicas=1
+  #helm install elasticsearch elastic/elasticsearch -n dapr-monitoring --set replicas=1
+
   echo "Waiting for elasticsearch start-up..."
   # One replica, so only wait for index 0
   kubectl wait --namespace=dapr-monitoring --for=condition=ready pod --timeout=600s -l statefulset.kubernetes.io/pod-name=elasticsearch-master-0
 
   # TODO: Load metricbeat and filebeat via helm chart.
 
-  helm install kibana elastic/kibana -n dapr-monitoring
+  # TODO: Download helm chart, replace image prefix with proxy variables, install.
+
+  
+  helm pull elastic/kibana --untar --untardir $MONITORING_TMP
+  sed -i "s|docker.elastic.co/|$DOCKER_REGISTRY_PROXY$REGISTRY_ELASTIC_DOCKER|g" $MONITORING_TMP/kibana/values.yaml
+  helm install kibana $MONITORING_TMP/kibana -n dapr-monitoring
+  #helm install kibana elastic/kibana -n dapr-monitoring
 
   echo "Waiting for kibana start-up..."
   kubectl wait --for=condition=Ready pods -l=app=kibana -n dapr-monitoring --timeout=600s
@@ -30,23 +44,30 @@ function enable_monitoring() {
   wget ${source_url}/${configMap} -O ${configMapPath}
   kubectl apply -f ${configMapPath}
 
-  daprFile=fluentd-dapr-with-rbac.yaml
-  daprFilePath=${MONITORING_TMP}/${daprFile}
-  wget ${source_url}/${daprFile} -O ${daprFilePath}
-  kubectl apply -f ${daprFilePath}
+  # YAML source: https://docs.dapr.io/docs/fluentd-dapr-with-rbac.yaml
+  local fluentdDaemonsetConfig=$MONITORING_TMP/fluentd-dapr-with-rbac.yaml
+  export FLUENTD_DAEMONSET_IMAGE=${DOCKER_REGISTRY_PROXY}${REGISTRY_DOCKER_IO}fluent/fluentd-kubernetes-daemonset:v1.9.2-debian-elasticsearch7-1.0
+  populateTemplate ../config/fluentd-dapr-with-rbac_template.yaml ${fluentdDaemonsetConfig}
+  kubectl apply -f ${fluentdDaemonsetConfig}
 
   kubectl wait --for=condition=Ready pods -l=k8s-app=fluentd-logging -n kube-system --timeout=600s
 
   helm repo add --force-update dapr https://dapr.github.io/helm-charts/
   helm repo update
-  helm install dapr dapr/dapr --namespace dapr-system --set global.logAsJson=true
+
+  # Nicely, there's a global variable for the registry setting
+  REGISTRY=${DOCKER_REGISTRY_PROXY}${REGISTRY_DOCKER_IO}
+  REGISTRY=${REGISTRY:-docker.io/}
+  
+  echo "Waiting for dapr-system installation and start-up..."
+  helm install dapr dapr/dapr --namespace dapr-system --set global.logAsJson=true --set global.registry="${REGISTRY}daprio" --wait
 
   echo "Waiting for dapr-system start-up..."
-  kubectl wait --for=condition=Ready pods -l=app=dapr-dashboard -n dapr-system --timeout=120s
-  kubectl wait --for=condition=Ready pods -l=app=dapr-operator -n dapr-system --timeout=120s
-  kubectl wait --for=condition=Ready pods -l=app=dapr-placement-server -n dapr-system --timeout=120s
-  kubectl wait --for=condition=Ready pods -l=app=dapr-sentry -n dapr-system --timeout=120s
-  kubectl wait --for=condition=Ready pods -l=app=dapr-sidecar-injector -n dapr-system --timeout=120s
+  # kubectl wait --for=condition=Ready pods -l=app=dapr-dashboard -n dapr-system --timeout=120s
+  # kubectl wait --for=condition=Ready pods -l=app=dapr-operator -n dapr-system --timeout=120s
+  # kubectl wait --for=condition=Ready pods -l=app=dapr-placement-server -n dapr-system --timeout=120s
+  # kubectl wait --for=condition=Ready pods -l=app=dapr-sentry -n dapr-system --timeout=120s
+  # kubectl wait --for=condition=Ready pods -l=app=dapr-sidecar-injector -n dapr-system --timeout=120s
 
   nohup kubectl port-forward svc/kibana-kibana 5601 -n dapr-monitoring > ${MONITORING_TMP}/port-forward.log 2>&1 &
 }
