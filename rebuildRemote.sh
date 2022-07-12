102bd0f6a3a54c3c7f3c21e43682d93d09785ea2
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-07-12 16:46:29 +0000

    Updating IaC configuration.

---
+++ rebuildRemote.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+pushd ~/git/ChaordicLedger/infrastructure
+
+if [ -z "$OS_PASSWORD_INPUT" ]
+then
+  echo "\$OS_PASSWORD_INPUT is empty; enter password:"
+  read -s OS_PASSWORD_INPUT
+
+  if [ -z "$OS_PASSWORD_INPUT" ]
+  then
+    echo "\$OS_PASSWORD_INPUT is empty; cannot proceed."
+    exit 1
+  fi
+else
+  echo "\$OS_PASSWORD_INPUT is set!"
+fi
+
+. .lmcoenv
+. .envfile
+
+mkdir -p $TERRAFORM_TMP_PATH
+pushd terraform
+
+terraform init -input=false
+terraform plan -out $TERRAFORM_PLAN_FILE -var="inventoryFile=$TF_VAR_inventoryFile" -input=false
+terraform apply -input=false $TERRAFORM_PLAN_FILE
+popd
+
+# waitTime=900
+# echo "Waiting $waitTime seconds for the first-boot autoconfig to finish."
+# sleep $waitTime
+
+export ANSIBLE_HOST_KEY_CHECKING=false
+ansible-playbook ansible/prerequisites.yml -i $TF_VAR_inventoryFile --key-file $CHAORDICLEDGER_TERRAFORM_KEYFILE
+ansible-playbook ansible/chaordicledger.yml -i $TF_VAR_inventoryFile --key-file $CHAORDICLEDGER_TERRAFORM_KEYFILE
+
+popd
