304d378cd077613e4bbf0a6b34819dafb1235c21
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-07-13 19:17:06 +0000

    Update IaC configuration.

--- infrastructure/runTerraform.sh
+++ infrastructure/runTerraform.sh
@@ -5,6 +5,8 @@ set -e
 . .lmcoenv
 . .envfile
 
+pushd terraform
+
 if [[ -d $TERRAFORM_TMP_PATH ]]
 then
   echo "Tearing-down existing environment."
@@ -14,11 +16,9 @@ then
 fi
 
 mkdir -p $TERRAFORM_TMP_PATH
-pushd terraform
 terraform init -input=false
 terraform plan -out $TERRAFORM_PLAN_FILE -var="inventoryFile=$TF_VAR_inventoryFile" -input=false
 terraform apply -input=false $TERRAFORM_PLAN_FILE
-popd
 
 waitTime=1200
 echo "Waiting $waitTime seconds for the first-boot autoconfig to finish."
@@ -26,3 +26,5 @@ sleep $waitTime
 
 echo "Done waiting. Check each host with 'ssh -i $CHAORDICLEDGER_TERRAFORM_KEYFILE <USER>@<IPAddress>' to ensure the first-boot behavior is complete."
 echo "(e.g.: check the ~/initial-maintenance.log* file to see if the play recap indicates 0 failed items.)"
+
+popd
