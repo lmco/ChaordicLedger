67578a9f4c11e5c5ee447c4b01ace2ba43b94dac
Michael F. Marchini <michael.f.marchini@lmco.com>
2022-12-11 19:02:27 +0000

    Added a sales system scenario.

--- runTestScenarios.sh
+++ runTestScenarios.sh
@@ -6,11 +6,15 @@ function orchestratorLog() {
   echo -e "[$now | Orchestrator | INFO] $1"
 }
 
+function waitForSeconds() {
+  wait=$1
+  orchestratorLog "Waiting ${wait} seconds"
+  sleep $wait
+}
+
 orchestratorLog "Orchestrator Started"
 
-wait=30
-orchestratorLog "Waiting ${wait} seconds to start tests"
-sleep $wait
+#waitForSeconds 30
 
 orchestratorLog "Commencing tests"
 
@@ -19,17 +23,35 @@ export ROOT_DIR=$(pwd)
 pushd scenarios
 
 ./generatedArtifacts/createOneArtifact.sh
+waitForSeconds 30
 
 pushd industrial/gpsExample
  ./gpsExample.sh
 popd
+waitForSeconds 30
+
+pushd industrial/salesSystem
+ ./salesSystem.sh
+popd
+waitForSeconds 30
 
 ./generatedArtifacts/createTenRandomlyRelatedArtifacts.sh
+waitForSeconds 30
+
 ./generatedArtifacts/createTwentyRandomlyRelatedArtifacts.sh
+waitForSeconds 30
+
 ./generatedArtifacts/createOneHundredRandomlyRelatedArtifacts.sh
+waitForSeconds 30
+
 ./generatedArtifacts/createTwoHundredRandomlyRelatedArtifactsOfVaryingSizes.sh
+waitForSeconds 30
+
 ./generatedArtifacts/createOneThousandUnrelatedArtifactsOfFixedSize.sh
+waitForSeconds 30
+
 ./generatedArtifacts/createFiveHundredRandomlyRelatedArtifactsOfVaryingSizes.sh
+waitForSeconds 30
 
 #./testCleanup.sh
 
