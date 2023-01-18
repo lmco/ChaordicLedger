db9dd4b2cc22e30342a84812f9f195666e4a204d
Michael F. Marchini <michael.f.marchini@lmco.com>
2023-01-18 19:23:01 +0000

    Updated IAC configruation.

---
+++ deployAndTest.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+function syslog() {
+  now=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+  echo "[$now] $1"
+}
+
+pushd infrastructure
+
+syslog "Establishing resources via Terraform"
+./runTerraform.sh
+
+syslog "Establishing platform via Ansible"
+./runAnsible.sh
+
+popd
+
+echo "[$now] Done."
