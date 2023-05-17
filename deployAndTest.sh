c890dd9c6f44811a5433208bb95d983466474789
Montana Mendy <montana@linux.com>
2023-05-17 11:52:52 -0700

    Cleanup shell scripts (#30)
    Format shell script to be cleaner.

--- deployAndTest.sh
+++ deployAndTest.sh
@@ -3,14 +3,13 @@
 set -e
 
 function syslog() {
-  now=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+  now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
   echo "[$now] $1"
 }
 
 pushd infrastructure
 
-if [ "${CL_RELOAD_ENV}" == "false" ];
-then
+if [ "${CL_RELOAD_ENV}" == "false" ]; then
   syslog "Not establishing resources via Terraform"
 else
   syslog "Establishing resources via Terraform"
