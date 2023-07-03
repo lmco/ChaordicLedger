e0e68d5b936b2381cb7d5d29afad34bb4d892889
Montana Mendy <montana@linux.com>
2023-07-03 16:03:03 -0700

    Fix the imports, yapf

--- metricsVisualizer.py
+++ metricsVisualizer.py
@@ -3,9 +3,10 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plotData(csvFile, column, prefix, outputdir):
-    df = pd.read_csv(csvFile)
-    outputFile=os.path.join(outputdir, f"{prefix}_{column.lower()}.png")
+
+def plot_data(csv_file, column, prefix, output_dir):
+    df = pd.read_csv(csv_file)
+    output_file = os.path.join(output_dir, f"{prefix}_{column.lower()}.png")
 
     pd.options.display.float_format = '{:.2f}'.format
 
@@ -21,20 +22,17 @@ def plotData(csvFile, column, prefix, outputdir):
     # Rotating X-axis labels
     plt.xticks(rotation=25)
 
-    lineplot.figure.savefig(outputFile)
+    lineplot.figure.savefig(output_file)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Visualizes resource usage samples as graphs.')
-    parser.add_argument('-i', '--input',
-                        help="The input CSV file.", required=True)
-    parser.add_argument('-c', '--column',
-                        help="The column to sum.", required=True)
-    parser.add_argument('-p', '--prefix',
-                        help="The output file's prefix.", required=True)
-    parser.add_argument('-o', '--outputdir',
-                        help="The output directory.", required=True)
+    parser.add_argument('-i', '--input', help="The input CSV file.", required=True)
+    parser.add_argument('-c', '--column', help="The column to sum.", required=True)
+    parser.add_argument('-p', '--prefix', help="The output file's prefix.", required=True)
+    parser.add_argument('-o', '--outputdir', help="The output directory.", required=True)
 
     args = parser.parse_args()
 
-    plotData(args.input, args.column, args.prefix, args.outputdir)
+    plot_data(args.input, args.column, args.prefix, args.outputdir)
