4b3528ca6110e3e89e29322aa0c7c793950e2dc2
Montana Mendy <montana@linux.com>
2023-07-13 07:50:17 -0700

    Refactor docker-entrypoint (#34)

--- chaincode/lightswitch/docker/docker-entrypoint.sh
+++ chaincode/lightswitch/docker/docker-entrypoint.sh
@@ -2,19 +2,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 set -euo pipefail
-: ${CORE_PEER_TLS_ENABLED:="false"}
-: ${DEBUG:="false"}
+
+CORE_PEER_TLS_ENABLED="${CORE_PEER_TLS_ENABLED:-false}"
+DEBUG="${DEBUG:-false}"
 
 env | sort
 
-if [ "${DEBUG,,}" = "true" ]; then
-   echo "DEBUG"
-   exec java -Djava.util.logging.config.file=logging.properties -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:8000 -jar /chaincode.jar
-elif [ "${CORE_PEER_TLS_ENABLED,,}" = "true" ]; then
-   echo "CORE_PEER_TLS_ENABLED=true"
-   exec java -Djava.util.logging.config.file=logging.properties -jar /chaincode.jar # todo
+if [[ "${DEBUG,,}" = "true" ]]; then
+    echo "DEBUG"
+    exec java -Djava.util.logging.config.file=logging.properties -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=localhost:8000 -jar /chaincode.jar
+elif [[ "${CORE_PEER_TLS_ENABLED,,}" = "true" ]]; then
+    echo "CORE_PEER_TLS_ENABLED=true"
+    exec java -Djava.util.logging.config.file=logging.properties -jar /chaincode.jar # todo
 else
-   echo "Nominal execution"
-   exec java -Djava.util.logging.config.file=logging.properties -jar /chaincode.jar
+    echo "Nominal execution"
+    exec java -Djava.util.logging.config.file=logging.properties -jar /chaincode.jar
 fi
