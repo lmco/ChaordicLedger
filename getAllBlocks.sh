dac8ed3d8f34f0df4eb95ebe5fb0a1c8af1e9f58
Montana Mendy <montana@linux.com>
2023-07-13 07:55:08 -0700

    Refactor to current standards. (#35)
    Using `curl` in a more simple manner for possibly retrieving Lockheed Martin dependencies, etc.

--- getAllBlocks.sh
+++ getAllBlocks.sh
@@ -13,32 +13,29 @@ rm $blockinfo/*
 latestBlock=$(curl -s -X GET "http://localhost:8080/v1/blockchain/getLatestBlock" -H "accept: application/json" | jq .header.number | tr -d '\"')
 prevFile=""
 
-while [ $blocknumber -le $latestBlock ]
-do
-  echo "Retrieving block number: ${blocknumber}"
-  blockname=block`printf %03d ${blocknumber}`
-  currentFile=$blockdir/${blockname}.json
-  currentResult=$(curl -s -X GET "http://localhost:8080/v1/blockchain/getNumberedBlock/${blocknumber}" -H "accept: application/json")
+while [ $blocknumber -le $latestBlock ]; do
+        echo "Retrieving block number: ${blocknumber}"
+        blockname=block$(printf %03d ${blocknumber})
+        currentFile=$blockdir/${blockname}.json
+        currentResult=$(curl -s -X GET "http://localhost:8080/v1/blockchain/getNumberedBlock/${blocknumber}" -H "accept: application/json")
 
-  if [ $? -ne 0 ]
-  then
-    echo "Error retrieving block ${blocknumber}: $currentResult"
-    break
-  fi
-  
-  # Validate
-  if [ "$(echo $currentResult | jq .unknown)" == "null" ] && [ "$(echo $currentResult | jq .error)" == "null" ]
-  then
-    echo "Writing block file $currentFile"
-    echo $currentResult | jq > $currentFile
-    cat $currentFile | jq .data.data[0].payload.header.channel_header > $blockinfo/${blockname}_info.json
-  else
-    echo "Block $blocknumber does not exist."
-    break
-  fi
+        if [ $? -ne 0 ]; then
+                echo "Error retrieving block ${blocknumber}: $currentResult"
+                break
+        fi
 
-  prevFile=$currentFile
-  blocknumber=$((blocknumber+1))
+        # Validate
+        if [ "$(echo $currentResult | jq .unknown)" == "null" ] && [ "$(echo $currentResult | jq .error)" == "null" ]; then
+                echo "Writing block file $currentFile"
+                echo $currentResult | jq >$currentFile
+                cat $currentFile | jq .data.data[0].payload.header.channel_header >$blockinfo/${blockname}_info.json
+        else
+                echo "Block $blocknumber does not exist."
+                break
+        fi
+
+        prevFile=$currentFile
+        blocknumber=$((blocknumber + 1))
 done
 
 totalblocks=$(($latestBlock + 1)) # Adjust for zero-based indexing.
