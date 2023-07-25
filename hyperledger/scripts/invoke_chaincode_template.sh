a05f0017bdb5d6bb421c3f7efea501e655a362d8
Montana Mendy <montana@linux.com>
2023-07-25 11:12:09 -0700

    Easier for annotations, refactor `msp` part, more readable format. (#36)
    Thank you Dr. Marchini!

--- hyperledger/scripts/invoke_chaincode_template.sh
+++ hyperledger/scripts/invoke_chaincode_template.sh
@@ -2,11 +2,11 @@
 set -x
 export CORE_PEER_ADDRESS=org1-peer1:7051
 peer chaincode \
-      invoke \
-      -o org0-orderer1:6050 \
-      --tls --cafile /var/hyperledger/fabric/organizations/ordererOrganizations/org0.example.com/msp/tlscacerts/org0-tls-ca.pem \
-      -n ${CHAINCODE_NAME} \
-      -C ${CHANNEL_NAME} \
-      -c '${parameters}'
+        invoke \
+        -o org0-orderer1:6050 \
+        --tls --cafile /var/hyperledger/fabric/organizations/ordererOrganizations/org0.example.com/msp/tlscacerts/org0-tls-ca.pem \
+        -n ${CHAINCODE_NAME} \
+        -C ${CHANNEL_NAME} \
+        -c '${parameters}'
 
 sleep 2
