c16aed0a9ddae3fc3be1487c3be747e8e3d506e0
Michael Mendy <montana@linux.com>
2023-08-25 16:13:17 -0700

    Push the refactor again.
    Sorry about that Dr. Marchini.

--- regenerateChaincode.sh
+++ regenerateChaincode.sh
@@ -1,10 +1,10 @@
-#/bin/sh
+#!/bin/sh
 
 set -e
 
 rm -rf ../Obsidian/target/*
 pushd ../Obsidian
-mv src/test src/test_save
+mv src/test src/test_save  
 mkdir src/test
 sbt assembly
 popd
@@ -15,7 +15,7 @@ pushd out/LightSwitch
 cp $DIR/build.gradle .
 pwd
 #gradle --no-daemon -Dhttp.proxyHost=proxy-zsgov.external.lmco.com -Dhttp.proxyPort=80 -Dhttps.proxyHost=proxy-zsgov.external.lmco.com -Dhttps.proxyPort=80 clean getDeps build
-gradle --no-daemon -Dhttp.proxyHost=proxy-zsgov.external.lmco.com -Dhttp.proxyPort=80 -Dhttps.proxyHost=proxy-zsgov.external.lmco.com -Dhttps.proxyPort=80 clean build shadowJar #-x checkstyleMain -x checkstyleTest
+gradle --no-daemon -Dhttp.proxyHost=proxy-zsgov.external.lmco.com -Dhttp.proxyPort=80 -Dhttps.proxyHost=proxy-zsgov.external.lmco.com -Dhttps.proxyPort=80 clean build shadowJar #-x checkstyleMain -x checkstyleTest 
 pushd build/libs
 
 java -classpath ./*.jar -jar fabric-chaincode-example-gradle-1.0-SNAPSHOT.jar
