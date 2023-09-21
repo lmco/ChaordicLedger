9c356a8940eb7573690c9347f574c76e291b965b
Michael Mendy <montana@linux.com>
2023-09-21 08:36:04 -0700

    Latest Python standards, refactoring. More room for annotation. (#42)

--- scenarios/industrial/inputProcessor.py
+++ scenarios/industrial/inputProcessor.py
@@ -6,15 +6,15 @@ from requests_toolbelt.multipart import encoder
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Processes the given inputs as part of a test scenario.')
-    parser.add_argument('-i', '--inputdir',
-                        help="The input directory.", required=True)
+        description="Processes the given inputs as part of a test scenario."
+    )
+    parser.add_argument("-i", "--inputdir", help="The input directory.", required=True)
 
     args = parser.parse_args()
 
     inputMap = None
 
-    with open(os.path.join(args.inputdir, "map.json"), 'r') as f:
+    with open(os.path.join(args.inputdir, "map.json"), "r") as f:
         inputMap = json.load(f)
 
     apiRoot = "http://localhost:8080/v1"
@@ -25,18 +25,15 @@ if __name__ == "__main__":
     responseMap = {}
 
     for file in inputMap.keys():
-        print(f"Uploading artifact \"{file}\" to {artifactUrl}.")
+        print(f'Uploading artifact "{file}" to {artifactUrl}.')
 
         session = requests.session()
-        with open(os.path.join(args.inputdir, file), 'rb') as f:
-            form = encoder.MultipartEncoder({
-                "upfile": (file, f, "application/octet-stream"),
-                "friendlyname": file
-            })
-            headers = {"Prefer": "respond-async",
-                       "Content-Type": form.content_type}
-            response = session.post(
-                artifactUrl, headers=headers, data=form.to_string())
+        with open(os.path.join(args.inputdir, file), "rb") as f:
+            form = encoder.MultipartEncoder(
+                {"upfile": (file, f, "application/octet-stream"), "friendlyname": file}
+            )
+            headers = {"Prefer": "respond-async", "Content-Type": form.content_type}
+            response = session.post(artifactUrl, headers=headers, data=form.to_string())
 
             data = json.loads(response.text)["result"]
             if data is None:
@@ -54,11 +51,11 @@ if __name__ == "__main__":
                 print(f"{sourceName}: {responseMap[relation]}")
                 targetName = responseMap[relation]["result"]["IPFSName"]
                 print(
-                    f"Submitting relationship between \"{file}\" (\"{sourceName}\") and \"{relation}\" (\"{targetName}\") to {relationshipUrl}.")
+                    f'Submitting relationship between "{file}" ("{sourceName}") and "{relation}" ("{targetName}") to {relationshipUrl}.'
+                )
 
-                response = requests.post(relationshipUrl, json={
-                    "nodeida": sourceName,
-                    "nodeidb": targetName
-                })
+                response = requests.post(
+                    relationshipUrl, json={"nodeida": sourceName, "nodeidb": targetName}
+                )
 
                 print(f"Code: {response.status_code} - {response.text}")
