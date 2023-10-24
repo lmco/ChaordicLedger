6a9afd84be6246398bc781afefd34728f069885b
Michael Mendy <montana@linux.com>
2023-10-24 07:54:40 -0700

    Simpler to read, possible performance enhancement. Refactored some of the lateral areas of the bash script. (#44)

--- hyperledger/scripts/concatenator.py
+++ hyperledger/scripts/concatenator.py
@@ -2,25 +2,26 @@ import argparse
 import json
 import os
 
-def concatenatePerConfiguration(inputdir: str, output:str):
+
+def concatenatePerConfiguration(inputdir: str, output: str):
     order = None
-    
+
     with open(os.path.join(inputdir, "concatenationConfig.json"), "r") as f:
         order = json.load(f)
-    
+
     with open(output, "w") as outfile:
         for file in order:
             with open(os.path.join(inputdir, file), "r") as f:
                 outfile.writelines(f.readlines())
                 outfile.writelines([os.linesep])
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Converts gathered Kubernetes metrics from tabular form to JSON.')
-    parser.add_argument('-i', '--inputdir',
-                        help="The input directory.", required=True)
-    parser.add_argument('-o', '--output',
-                        help="The output file.", required=True)
+        description="Converts gathered Kubernetes metrics from tabular form to JSON."
+    )
+    parser.add_argument("-i", "--inputdir", help="The input directory.", required=True)
+    parser.add_argument("-o", "--output", help="The output file.", required=True)
 
     args = parser.parse_args()
 
